b03ce26ecafa8575488aa1c8ba2a589481f2b643
yangkky <seinchin@gmail.com>
2020-05-07 15:55:06 -0400

    Put down some useful alphabets in constants.

---
+++ sequence_models/constants.py
@@ -0,0 +1,25 @@
+#  It's helpful to separate out the twenty canonical amino acids from the rest
+CAN_AAS = 'ACDEFGHIKLMNPQRSTVW'
+AMB_AAS = 'BZX'
+OTHER_AAS = 'JOU'
+ALL_AAS = CAN_AAS + AMB_AAS + OTHER_AAS
+
+DNA = 'GATC'
+EXTENDED_NA = 'RYWSMKHBVDN'
+RNA = 'GAUC'
+IUPAC_AMB_DNA = DNA + EXTENDED_NA
+IUPAC_AMB_RNA = RNA + EXTENDED_NA
+NAS = 'GATUC' + EXTENDED_NA
+
+STOP = '*'
+GAP = '-'
+MASK = '#'  # Useful for masked language model training
+START = '@'
+
+SPECIALS = STOP + GAP + MASK + START
+PROTEIN_ALPHABET = ALL_AAS + SPECIALS
+
+IUPAC_SS = 'HSTC'
+DSSP = 'GHITEBSC'
+SS8 = DSSP
+SS3 = 'HSL'  # H: GHI; S: EB; L: STC
