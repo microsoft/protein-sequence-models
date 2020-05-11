3b1befd4bf38f12ec74c71d9d269c1ba0fd34441
yangkky <seinchin@gmail.com>
2020-05-11 11:38:40 -0400

    Get VAE loss working.

---
+++ sequence_models/losses.py
@@ -0,0 +1,86 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+
+class SequenceCrossEntropyLoss(nn.Module):
+    """Cross-entropy loss for sequences. """
+
+    def __init__(self, weight=None, ignore_index=-100):
+        super(SequenceCrossEntropyLoss, self).__init__()
+        self.class_weights = weight  # These are class weights
+        self.ignore_index = ignore_index
+
+    def forward(self, prediction, tgt, reduction='mean'):
+        return F.cross_entropy(prediction.transpose(1, 2), tgt, weight=self.class_weights, reduction=reduction,
+                               ignore_index=self.ignore_index)
+
+
+class MaskedCrossEntropyLoss(nn.Module):
+    """Masked cross-entropy loss for sequences.
+
+    Evaluates the cross-entropy loss at specified locations in a sequence.
+
+    Shape:
+        Inputs:
+            - pred: (N, L, n_tokens)
+            - tgt: (N, L)
+            - mask: (N, L)
+            - weight: (C, ): class weights for nn.CrossEntropyLoss
+    """
+
+    def __init__(self, weight=None):
+        super(MaskedCrossEntropyLoss, self).__init__()
+        self.cel = nn.CrossEntropyLoss(weight=weight, reduction='none')
+
+    def forward(self, pred, tgt, mask):
+        _, _, n_tokens = pred.size()
+        mask = mask.float()
+        loss = self.cel(pred.contiguous().view(-1, n_tokens), tgt.contiguous().view(-1))
+        loss = (mask.view(-1) * loss).sum() / mask.sum()
+        return loss
+
+
+class VAELoss(nn.Module):
+    """A simple VAE loss.
+
+    This is the sum of a reconstruction loss (calculated on predictions and ground truths)
+    and the KL divergence between z (mu, log_var) and a standard normal.
+
+    Args:
+        class_weights (): The reconstruction loss
+
+    Inputs:
+        pre: The predictions (N, *)
+        tgt: The ground truths (N, *)
+        mu: Predicted means for the latent space
+        log_var: Predicted log variance for the latent space
+        beta: Ratio between the reconstruction and KLD losses. Optional: default is 1.0.
+        sample_weights: Weight to place on each sample. Default None. Size (N, 1 x *)
+        reduction (str): 'mean' or 'none'
+
+    Outputs:
+        loss (, ): Tensor containing the VAE loss
+    """
+
+    def __init__(self, class_weights=None):
+        super(VAELoss, self).__init__()
+        self.recon_loss = SequenceCrossEntropyLoss(weight=class_weights)
+
+    def forward(self, pre, tgt, mu, log_var, beta=1.0, sample_weights=None, reduction='mean'):
+        kld = -0.5 * (1 + log_var - mu ** 2 - log_var.exp())
+        r_loss = self.recon_loss(pre, tgt, reduction='none')
+        if sample_weights is None:
+            kld = kld.mean(dim=1)
+            r_loss = r_loss.mean(dim=1)
+        else:
+            kld = (kld * sample_weights).mean(dim=1)
+            r_loss *= sample_weights
+            if self.recon_loss.class_weights is not None:
+                r_loss = r_loss.sum(dim=1) / self.recon_loss.class_weights[tgt].sum()
+            else:
+                r_loss = r_loss.mean(dim=1)
+        if reduction == 'mean':
+            kld = kld.mean()
+            r_loss = r_loss.mean()
+        return r_loss + beta * kld, r_loss, kld
---
+++ tests/vae_test.py
@@ -0,0 +1,110 @@
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+from sequence_models.losses import VAELoss, SequenceCrossEntropyLoss
+from sequence_models.vae import FCDecoder, FCEncoder, VAE
+
+
+N = 2
+L = 5
+D = 3
+
+src = [
+    [0, 1, 2, 0, 1],
+    [0, 1, 1, 1, 0]
+]
+src = torch.LongTensor(src)
+
+n_hidden = np.random.choice(np.arange(3, 10))
+d_h = list(np.random.choice(np.arange(1, 10), size=n_hidden))
+d_z = np.random.choice(np.arange(2, 10))
+encoder = FCEncoder(L, D, d_h, d_z)
+decoder = FCDecoder(L, D, d_h[::-1], d_z)
+
+
+def test_encoder():
+    assert encoder.embedder.num_embeddings == D
+    assert encoder.embedder.embedding_dim == d_h[0]
+    mu, logvar = encoder(src)
+    nm, dm = mu.size()
+    nv, dv = logvar.size()
+    assert nm == nv
+    assert nm == N
+    assert dm == d_z
+    assert dv == d_z
+
+
+def test_decoder():
+    z = torch.Tensor(np.random.random((N, d_z)))
+    p = decoder(z)
+    n, ell, dp = p.size()
+    assert n == N
+    assert ell == L
+    assert dp == D
+
+
+def test_vae():
+    vae = VAE(encoder, decoder)
+    p, mu, logvar = vae(src)
+    mu2, logvar2 = encoder(src)
+    assert torch.allclose(mu2, mu)
+    assert torch.allclose(logvar, logvar2)
+    # Check shape of p
+    n, ell, d = p.size()
+    assert n == N
+    assert ell == L
+    assert d == D
+    # Test encoder
+    m1, s1 = vae.encode(src)
+    m2, s2 = encoder(src)
+    assert torch.allclose(m1, m2)
+    assert torch.allclose(s1, s2)
+    # Test decode
+    z = torch.Tensor(np.random.random((N, d_z)))
+    p1 = decoder(z)
+    p2 = vae.decode(z)
+    assert torch.allclose(p1, p2)
+
+
+def test_loss():
+    r_loss_func = SequenceCrossEntropyLoss()
+    vae = VAE(encoder, decoder)
+    p, mu, logvar = vae(src)
+    r_loss = r_loss_func(p, src, reduction='none')
+    kl_loss = -0.5 * (1 + logvar - mu ** 2 - logvar.exp())
+    beta = torch.rand(1)
+
+    # Without classweights or sample_weights
+    vloss = VAELoss(class_weights=None)
+    # With reduction
+    loss, r, k = vloss(p, src, mu, logvar, beta=beta)
+    assert torch.allclose(r_loss.mean(dim=1).mean(dim=0) + beta * kl_loss.mean(dim=1).mean(dim=0), loss)
+    assert torch.allclose(r, r_loss.mean(dim=1).mean())
+    assert torch.allclose(k, kl_loss.mean(dim=1).mean())
+    # Without reduction
+    loss, r, k = vloss(p, src, mu, logvar, beta=beta, reduction='none')
+    assert torch.allclose(r_loss.mean(dim=1) + beta * kl_loss.mean(dim=1), loss)
+    assert torch.allclose(r, r_loss.mean(dim=1))
+    assert torch.allclose(k, kl_loss.mean(dim=1))
+
+    # With class_weights and sample_weights
+    cw = torch.rand(3)
+    sw = torch.rand((N, 1))
+    r_loss_func = SequenceCrossEntropyLoss(weight=cw)
+    r_loss = r_loss_func(p, src, reduction='none')
+    r_loss *= sw
+    r_loss = r_loss.sum(dim=1) / r_loss_func.class_weights[src].sum()
+    kl_loss *= sw
+    vloss = VAELoss(class_weights=cw)
+    # With reduction
+    loss, r, k = vloss(p, src, mu, logvar, beta=beta, sample_weights=sw)
+    assert torch.allclose(r_loss.mean() + beta * kl_loss.mean(dim=1).mean(dim=0), loss)
+    assert torch.allclose(r, r_loss.mean())
+    assert torch.allclose(k, kl_loss.mean(dim=1).mean())
+    # Without reduction
+    loss, r, k = vloss(p, src, mu, logvar, beta=beta, sample_weights=sw, reduction='none')
+    assert torch.allclose(r_loss + beta * kl_loss.mean(dim=1), loss)
+    assert torch.allclose(r, r_loss)
+    assert torch.allclose(k, kl_loss.mean(dim=1))
