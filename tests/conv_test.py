080f3f95113e8f3f9247694969233e23558c333b
Kevin Kaichuang Yang <kevyan@microsoft.com>
2020-06-04 13:38:38 -0400

    Test causal convolutions.

--- tests/conv_test.py
+++ tests/conv_test.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
-from sequence_models.convolutional import ByteNetBlock, ByteNet, ConditionedByteNetDecoder
+from sequence_models.convolutional import ByteNetBlock, ByteNet, ConditionedByteNetDecoder, \
+    HierarchicalCausalConv1d, MaskedCausalConv1d
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -33,3 +35,43 @@ assert out.shape == (b, sum(ells), d)
 net = ByteNet(n_tokens, d_e, d, n_layers, 3, r).to(device)
 out = net(x)
 assert out.shape == (b, ell, d)
+
+# Test Causal convolution
+din = 8
+dout = 9
+k = 3
+n = 5
+ells = [3, 3, 4, 4, 10, 5, 10]
+dil = 4
+x = torch.randn(n, sum(ells), din).to(device)
+layer = MaskedCausalConv1d(din, dout, k, dilation=dil).to(device)
+x.requires_grad = True
+out = layer(x)
+pos = 20
+loss = out[0, pos, :].sum()
+grad = torch.autograd.grad(loss, x, retain_graph=True)[0][0].sum(dim=1)
+start = pos - (k - 1) * dil
+for p in range(start, sum(ells)):
+    if p > pos:
+        assert grad[p].sum() == 0
+    elif (p - start) % dil == 0:
+        assert grad[p].sum() != 0
+
+# Test gradients in hierarchical causal convolution
+dil = 4
+layer = HierarchicalCausalConv1d(din, dout, ells, k, dilation=dil).to(device)
+x = torch.randn(n, sum(ells), din).to(device)
+x.requires_grad = True
+out = layer(x)
+start = pos - (k - 1) * dil
+loss = out[0, pos, :].sum()
+grad = torch.autograd.grad(loss, x, retain_graph=True)[0][0].sum(dim=1)
+blocks = np.zeros(sum(ells))
+for ell in np.cumsum(ells):
+    blocks[ell:] += 1
+for p in range(start, sum(ells)):
+    if p > pos:
+        assert grad[p].sum() == 0
+    elif (p - start) % dil == 0:
+        if blocks[p] == blocks[pos]:
+            assert grad[p].sum() != 0
