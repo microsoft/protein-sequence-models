6967ab99ae5500b56a96b5053505c866f21682e3
hyeh20 <hughyeh1@gmail.com>
2020-10-29 17:43:02 -0400

    fixing 1d attention

--- sequence_models/structure.py
+++ sequence_models/structure.py
@@ -22,32 +22,58 @@ class Attention2d(nn.Module):
         return out
 
 
+# class Attention1d(nn.Module):
+#
+#     def __init__(self, L, h_dim):
+#         super().__init__()
+#         self.linear = nn.Linear(L*h_dim, L*h_dim)
+#
+#     def forward(self, x, input_mask=None):
+#         """
+#         x : torch.Tensor, (N, L, h_dim)
+#             input tensor
+#
+#         input_mask : torch.Tensor, (N, L)
+#             to mask specific residues or start/stop token
+#
+#         """
+#         n, el, h_dim = x.shape
+#         x = x.view(-1)
+#         attn = self.linear(x)
+#         if input_mask is not None:
+#             attn = attn.masked_fill_(~input_mask.repeat(n,h_dim,1).T.reshape(-1,).bool(),
+#                                      float('-inf'))
+#         attn = F.softmax(attn, dim=-1)
+#         out = (attn * x)
+#         return out
+
+
 class Attention1d(nn.Module):
-    
-    def __init__(self, L, h_dim):
+
+    def __init__(self, in_dim):
         super().__init__()
-        self.linear = nn.Linear(L*h_dim, L*h_dim)
-        
+        self.layer = MaskedConv2d(in_dim, 1, 1)
+
     def forward(self, x, input_mask=None):
         """
         x : torch.Tensor, (N, L, h_dim)
             input tensor
-            
+
         input_mask : torch.Tensor, (N, L)
-            to mask specific residues or start/stop token 
-            
+            to mask specific residues or start/stop token
+
         """
-        n, el, h_dim = x.shape
-        x = x.view(-1)
-        attn = self.linear(x)
+        n, ell, _, = x.shape
+        x = x.view(n, ell, 1, -1)
+        attn = self.layer(x)
+        attn = attn.view(n, -1)
         if input_mask is not None:
-            attn = attn.masked_fill_(~input_mask.repeat(n,h_dim,1).T.reshape(-1,).bool(), 
-                                     float('-inf'))
-        attn = F.softmax(attn, dim=-1)
-        out = (attn * x)
+            attn = attn.masked_fill_(~input_mask.view(n, -1).bool(), float('-inf'))
+        attn = F.softmax(attn, dim=-1).view(n, -1, 1)
+        out = (attn * x.view(n, ell, -1)).mean(dim=1)
         return out
-    
-    
+
+
 class StructureConditioner(nn.Module):
 
     def __init__(self, d_in, d_model, n_layers, kernel_size, r, dropout=0.0):
