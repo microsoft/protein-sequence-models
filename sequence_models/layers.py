ce81248a55c509b3fb94a556b18623eb3a73c5ca
Kevin Kaichuang Yang <kevyan@microsoft.com>
2021-04-30 14:18:33 -0400

    Add positional embedding layer.

--- sequence_models/layers.py
+++ sequence_models/layers.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import math
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
@@ -75,3 +76,22 @@ class FCStack(nn.Sequential):
                 layers.append(nn.Dropout(p))
             layers.append(nn.ReLU(inplace=True))
         super().__init__(*layers)
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.0, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, x.size(0), :]
+        return self.dropout(x)
