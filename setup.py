86b230ae68bf812f3ec2d94ec4637377e289fa25
Kevin Kaichuang Yang <kevyan@microsoft.com>
2022-03-18 14:28:02 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sequence-models",
-    version="0.0.1",
+    version="1.0.0",
     author="Kevin Yang",
     author_email="yang.kevin@microsoft.com",
-    description="Machine learning sequence_models for sequences.",
+    description="Machine learning for sequences.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://msrne-ml.visualstudio.com/sequence_models",
+    url="https://github.com/microsoft/protein-sequence-models",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
