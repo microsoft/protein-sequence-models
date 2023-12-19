6f77721d0a1100b4917a4dcd7f72209eed22352f
Kevin Kaichuang Yang <kevyan@microsoft.com>
2023-12-19 11:42:38 -0500

    Add option not to tie weights in ESM models.

--- sequence_models/esm.py
+++ sequence_models/esm.py
@@ -40,7 +40,7 @@ class ESM1b(nn.Module):
 
     def __init__(self, d_model, d_hidden, n_layers, n_heads, n_tokens=len(PROTEIN_ALPHABET),
                  padding_idx=PROTEIN_ALPHABET.index(PAD), mask_idx=PROTEIN_ALPHABET.index(MASK),
-                 max_positions=1024):
+                 max_positions=1024, tie_weights=True):
         super(ESM1b, self).__init__()
         self.embed_tokens = nn.Embedding(
             n_tokens, d_model, padding_idx=mask_idx
@@ -60,11 +60,18 @@ class ESM1b(nn.Module):
         self.embed_positions = LearnedPositionalEmbedding(max_positions, d_model, padding_idx)
         self.emb_layer_norm_before = ESM1bLayerNorm(d_model)
         self.emb_layer_norm_after = ESM1bLayerNorm(d_model)
-        self.lm_head = RobertaLMHead(
-            embed_dim=d_model,
-            output_dim=n_tokens,
-            weight=self.embed_tokens.weight
-        )
+        if tie_weights:
+            self.lm_head = RobertaLMHead(
+                embed_dim=d_model,
+                output_dim=n_tokens,
+                weight=self.embed_tokens.weight
+            )
+        else:
+            self.lm_head = RobertaLMHead(
+                embed_dim=d_model,
+                output_dim=n_tokens,
+                weight=nn.Linear(d_model, n_tokens).weight
+            )
 
     def forward(self, tokens):
 
@@ -72,15 +79,6 @@ class ESM1b(nn.Module):
         padding_mask = tokens.eq(self.padding_idx)  # B, T
 
         x = self.embed_tokens(tokens)
-
-        # if getattr(self.args, 'token_dropout', False):
-        #     x.masked_fill_((tokens == self.mask_idx).unsqueeze(-1), 0.0)
-        #     # x: B x T x C
-        #     mask_ratio_train = 0.15 * 0.8
-        #     src_lengths = (~padding_mask).sum(-1)
-        #     mask_ratio_observed = (tokens == self.mask_idx).sum(-1).float() / src_lengths
-        #     x = x * (1 - mask_ratio_train) / (1 - mask_ratio_observed)[:, None, None]
-
         x = x + self.embed_positions(tokens)
 
         x = self.emb_layer_norm_before(x)
@@ -119,7 +117,7 @@ class MSATransformer(nn.Module):
 
     def __init__(self, d_model, d_hidden, n_layers, n_heads, use_ckpt=False, n_tokens=len(PROTEIN_ALPHABET),
                  padding_idx=PROTEIN_ALPHABET.index(PAD), mask_idx=PROTEIN_ALPHABET.index(MASK),
-                 max_positions=1024):
+                 max_positions=1024, tie_weights=True):
         super(MSATransformer, self).__init__()
         self.embed_tokens = nn.Embedding(
             n_tokens, d_model, padding_idx=mask_idx
@@ -138,11 +136,18 @@ class MSATransformer(nn.Module):
         self.embed_positions = LearnedPositionalEmbedding(max_positions, d_model, padding_idx)
         self.emb_layer_norm_before = nn.LayerNorm(d_model)
         self.emb_layer_norm_after = nn.LayerNorm(d_model)
-        self.lm_head = RobertaLMHead(
-            embed_dim=d_model,
-            output_dim=n_tokens,
-            weight=self.embed_tokens.weight
-        )
+        if tie_weights:
+            self.lm_head = RobertaLMHead(
+                embed_dim=d_model,
+                output_dim=n_tokens,
+                weight=self.embed_tokens.weight
+            )
+        else:
+            self.lm_head = RobertaLMHead(
+                embed_dim=d_model,
+                output_dim=n_tokens,
+                weight=nn.Linear(d_model, n_tokens).weight
+            )
 
         self.use_ckpt = use_ckpt
 
