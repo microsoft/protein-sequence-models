7cf76442b4bb62f9b01df13f0b595a5d774db7a9
Kevin Kaichuang Yang <kevyan@microsoft.com>
2024-03-13 15:14:10 -0400

    Add collater for scl.

--- sequence_models/flip_utils.py
+++ sequence_models/flip_utils.py
@@ -12,6 +12,23 @@ def load_flip_data(data_fpath, dataset, split, max_len=2048):
     path = datadir + split + '.csv'
     df = pd.read_csv(path)
     df['sequence'] = df.sequence.apply(lambda s: re.sub(r'[^A-Z]', '', s.upper()))  # remove special characters
+    targets = []
+    if dataset == 'scl':
+        locations = {
+            'Cytoplasm': 0,
+            'Nucleus': 1,
+            'Cell membrane': 2,
+            'Mitochondrion': 3,
+            'Endoplasmic reticulum': 4,
+            'Lysosome/Vacuole': 5,
+            'Golgi apparatus': 6,
+            'Peroxisome': 7,
+            'Extracellular': 8,
+            'Plastid': 9
+        }
+        for i, row in df.iterrows():
+            targets.append(locations[row['target']])
+    df['target'] = targets
     test = df[df.set == 'test']
     train = df[(df.set == 'train') & (df.validation.isnull())]
     valid = df[~df.validation.isnull()]
